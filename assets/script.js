8ae69af18dee3fe4449c72dc951cee3d6ae1208a
Ginger Owen <gingerowen0421@gmail.com>
2022-06-03 20:54:28 -0600

    got time blocks

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,6 @@ const container = $('.container');
 
 const todayDate = document.getElementById("currentDay");
 
-//hours in work day, but do need to change to military time?
-var workDay = [9, 10, 11, 12, 1, 2, 3, 4];
-
 setInterval(time, 1000);
 
 //display current date and time
@@ -13,39 +10,52 @@ function time() {
     $(todayDate).text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 }
 
+//current time
+var thisMoment = moment().format('LT');
+
+
 //create function to see if current day and time is within desired range
 //will need range but do i need start and end  as separate variables?
 //will need variable to compare to
-
-let workWeek = moment().format('dddd'); //gives day of week monday, tuesday, wednesday...
-
-if (workWeek === Monday ||
-    workWeek === Tuesday ||
-    workWeek === Wednesday||
-    workWeek === Thursday||
-    workWeek === Friday
-    ) {
-    //allow display and text input
-} else {
-    "It's the weekend!"
-}
 
 //hour, text, submit
 //colstart is time
 //colMiddle is for text input
 //colEnd has not been assigned
-for (let block = 0; block <= 8; block++) {
+function timeBlock(block){
     const row = $('<div>').attr('class', 'row');
-    const colStart = $('<div>').attr('class', 'col-sm-2');
-    const colMiddle = $('<div>').attr('class', 'col-md-8');
-    const colEnd = $('<div>').attr('class', 'col-sm-2');
-    row.append(colStart);
-    row.append(colMiddle);
-    row.append(colEnd);
-    container.append(row);
+    const colStart = $('<div>').attr('class', 'col-sm-2 border');
+    if (block > 12) {
+        colStart.text(block - 12)
+    } else{
+        colStart.text(block);
+    }
+    const colMiddle = $('<textarea>').attr('class', 'col-md-8 border');
+    colMiddle.attr('id', 'task'); //create id for savinf to loc stor
+    const button = $('<button>').attr('id', 'button');
+    const colEnd = $('<div>').attr('class', 'col-sm-2 border');
+    $(colEnd).append(button);
+    $(row).append(colStart);
+    $(row).append(colMiddle);
+    $(row).append(colEnd);
+    return row;
+    console.log(container);
 }
 
+function getHours(params) {
+for (let block = 9; block <= 17; block++){
+$(container).append(timeBlock(block))
+}
+}
 
+getHours();
+
+const save = document.getElementById('button');
+save.addEventListener('click', function () {
+    localStorage.setItem('key', 'button')
+})
+
+console.log($(container).children());
 
 
 //create containers for each one hour time block
